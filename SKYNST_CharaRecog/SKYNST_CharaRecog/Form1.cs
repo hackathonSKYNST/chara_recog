4dedbb2c1e9511c8b364a4200a1ef3002b0b6eee
tigrig29 <tigrig.crex@gmail.com>
2016-11-01 14:43:49 +0900

    ソースコードに各空間を追加

--- SKYNST_CharaRecog/SKYNST_CharaRecog/Form1.cs
+++ SKYNST_CharaRecog/SKYNST_CharaRecog/Form1.cs
@@ -13,28 +13,337 @@ namespace SKYNST_CharaRecog
     public partial class Form1 : Form
     {
         public Form1()
-        {
+        {//16行目
             InitializeComponent();
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }//68行目
 
         private void button_webcam_Click(object sender, EventArgs e)
-        {
+        {//71行目
 
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }//123行目
 
         private void button_brows_Click(object sender, EventArgs e)
-        {
+        {//126行目
 
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }//178行目
 
         private void button_start_Click(object sender, EventArgs e)
-        {
+        {//181行目
 
-        }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }//233行目
+        
         private void button_output_Click(object sender, EventArgs e)
-        {
+        {//236行目
 
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }//288行目
+
+        //================ 以下、自作のクラス ================
+        
+
+        //●文字認識を行うクラス
+        private string chara_recog()
+        {
+            string str = "";
+
+            return str;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }//347行目
     }
 }
